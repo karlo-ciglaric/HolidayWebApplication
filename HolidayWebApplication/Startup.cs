69ef9aee7c0b7b4cad3c9d21b0984dba039acd88
Karlo Ciglaric <karlo.ciglaric@outlook.ie>
2020-02-13 19:14:16 +0000

    Testing Branch Creation

--- HolidayWebApplication/Startup.cs
+++ HolidayWebApplication/Startup.cs
@@ -20,7 +20,7 @@ namespace HolidayWebApplication
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; } // Testing
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
