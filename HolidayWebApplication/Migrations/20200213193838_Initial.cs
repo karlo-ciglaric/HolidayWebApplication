d0f7255b204bd7a49cbe710d16bf8639771ef2c9
Karlo Ciglaric <karlo.ciglaric@outlook.ie>
2020-02-13 19:41:36 +0000

    DbCreated

---
+++ HolidayWebApplication/Migrations/20200213193838_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HolidayWebApplication.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Destination = table.Column<string>(nullable: true),
+                    Cost = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    DepartureDate = table.Column<DateTime>(nullable: false),
+                    NoOfDays = table.Column<int>(nullable: false),
+                    Available = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+        }
+    }
+}
